5a71af2a7f6c6997b98aa0dfff777456e07e5d6d
Vaishnavi Sen <vaishusen2002@gmail.com>
2024-02-20 21:20:25 -0800

    merge conflict

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+import logging
+
+app = Flask(__name__)
+# to disable the logs that are printed on terminal
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# implementation of additional pages in flask
+    # @app.route('/add-and-multiply', methods=['POST']) 
+    # def add_and_multiply(): 
+    #     data = request.get_json()  # retrieve the data sent from JavaScript 
+    #     x = float(data.get('x', 0))  # convert to float, default to 0 if not present
+    #     y = float(data.get('y', 0))
+    #     result = (x + y) * 2
+    #     return jsonify(result=result) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
