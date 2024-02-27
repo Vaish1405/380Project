c86a5e61dafb544c9275ba0bd98c33ecd50a5ff6
Vaishnavi Sen <vaishusen2002@gmail.com>
2024-02-27 13:21:47 -0800

    test functionality for availableRooms added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import csv
 
-app = Flask(__name__)
+csv_file = 'AvailableRooms.csv'
 
-CSV_FILE = 'AvailableRooms.csv'
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -19,7 +19,13 @@ def rooms():
 
 @app.route('/available-rooms')
 def availableRooms():
-    return f"rooms available"
+    with open(csv_file, 'r') as file:
+        available_rooms = csv.DictReader(file)
+        for row in available_rooms:
+            if row['Available'] == 'True':
+                print(f"{row['RoomNumber']}")
+
+    return render_template('temp.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
