22f141e0ac981701421e8610cc39adf2faee384c
Vaishnavi Sen <vaishusen2002@gmail.com>
2024-02-29 13:23:04 -0800

    added extras page and functionality

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from roomsAvailability import find_available_rooms
 
 available_room_types = [] # to store the values read from csv file
 
 app = Flask(__name__)
+app.secret_key = "Vaish"
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -20,13 +22,20 @@ def rooms():
 def dateSelection(): 
     return render_template('dateSelection.html')
 
-@app.route('/room-selection', methods=['GET'])
+@app.route('/room-selection', methods=['POST'])
 def roomSelection():    
-    return render_template('room-selection.html', available_rooms=find_available_rooms())
+    session['check_in'] = request.form.get('check-in')
+    session['check_out'] = request.form.get('check-out')
+    session['people'] = request.form.get('people')
+    return render_template('room-selection.html', available_rooms=find_available_rooms(), form=request.form)
 
-@app.route('/extraSelection')
+@app.route('/extraSelection', methods=['GET'])
 def extraSelection(): 
-    return render_template('extraSelection.html')
+    return render_template('extraSelection.html', info=session)
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
