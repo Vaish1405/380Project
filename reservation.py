0417a0bb6fa6016bf722989b549374366f53dabf
Kaung Khant <123704203+kaung-k74@users.noreply.github.com>
2024-04-15 11:05:00 -0700

    Removed user_id

--- reservation.py
+++ reservation.py
@@ -2,31 +2,32 @@ import csv
 from roomsAvailability import read_info, csv_file
 from datetime import datetime, timedelta
 
-# defines the 
+
+# Defining the required data (attributes) for making reservation
 class Reservation:
-    def __init__(self, user_id, user_name, check_in, check_out, room_type):
-        self.user_id = user_id
+    def __init__(self, user_name, check_in, check_out, room_type):
         self.user_name = user_name
         self.check_in = check_in
         self.check_out = check_out
         self.room_type = room_type
 
+
 # Adding the reservation to database
 def make_reservation(reservation):
     # Declaring keys in dictionary that is going to be added to CSV file
-    main_key = ['user_id', 'user_name', 'check_in', 'check_out', 'room_type']
+    main_key = ['user_name', 'check_in', 'check_out', 'room_type']
     # Using dictionary to add the data to CSV file, let me know if you want alternative way
-    with open('reservations.csv', mode='a', newline='') as file:
-        data_adding = csv.DictWriter(file, fieldnames= main_key)
-        data_adding.writerow({'user_id' : reservation.user_id,
-                            'user_name': reservation.user_name,
-                            'check_in': reservation.check_in,
-                            'check_out': reservation.check_out,
-                            'room_type': reservation.room_type})
+    with open('reservations.csv', mode='a') as file:
+        data_adding = csv.DictWriter(file, fieldnames=main_key)
+        data_adding.writerow({'user_name': reservation.user_name,
+                              'check_in': reservation.check_in,
+                              'check_out': reservation.check_out,
+                              'room_type': reservation.room_type})
     return "successful"
 
+
 # based on front-end info, it will change room availability after making reservation
-def change_availability(self, room_type):
+def change_availability(room_type):
     # Reusing read_info method in roomsAvailability.py to read the data
     read_data = read_info()
     for r in read_data:
@@ -38,6 +39,7 @@ def change_availability(self, room_type):
         csv_writer = csv.DictWriter(file, fieldnames=field)
         csv_writer.writerows([r for r in read_data if r['RoomType'] == room_type])
 
+
 # Reservation controller to be called from front-end
 class ReservationController:
     def __init__(self, reservation):
@@ -46,20 +48,19 @@ class ReservationController:
         self.change_availability()
 
     def make_reservation(self):
-        main_key = ['user_id','user_name', 'check_in', 'check_out', 'room_type']
+        main_key = ['user_name', 'check_in', 'check_out', 'room_type']
         # Using dictionary to add the data to CSV file, let me know if you want alternative way
         with open('reservations.csv', mode='a', newline='') as file:
-            data_adding = csv.DictWriter(file, fieldnames= main_key)
-            data_adding.writerow({'user_id': self.reservation.user_id,
-                                'user_name': self.reservation.user_name,
-                                'check_in': self.reservation.check_in,
-                                'check_out': self.reservation.check_out,
-                                'room_type': self.reservation.room_type})
+            data_adding = csv.DictWriter(file, fieldnames=main_key)
+            data_adding.writerow({'user_name': self.reservation.user_name,
+                                  'check_in': self.reservation.check_in,
+                                  'check_out': self.reservation.check_out,
+                                  'room_type': self.reservation.room_type})
         return self.reservation
-    
+
     def change_availability(self):
-    # read data and change the 
-        with open('AvailableRooms.csv', mode="r") as file: 
+        # read data and change the
+        with open('AvailableRooms.csv', mode="r") as file:
             read_data = csv.reader(file)
             header = next(read_data)
             rows = []
@@ -75,8 +76,7 @@ class ReservationController:
                 rows.append(row)
 
         # write the updated data back to the csv file
-        with open('AvailableRooms.csv', mode='w') as file: 
+        with open('AvailableRooms.csv', mode='w') as file:
             write_data = csv.writer(file)
             write_data.writerow(header)
             write_data.writerows(rows)
-        
